5848458e00f2ce6077924b203cba991e7f16e4b3
Eduardo Lozano <eduardo.lozano@dracinternet.com>
2016-08-13 23:56:23 +0200

    Looking for the Gestures to solve a bug with scrolling the screen.

--- ScrollViews/ViewController.swift
+++ ScrollViews/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var images = [UIImageView]()
+    let frameWidth: CGFloat = 150.0
+    let frameHeight: CGFloat = 150.0
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        var contentWidth: CGFloat = 0.0
+        let scrollWidth = scrollView.frame.size.width
+        let scrollHeight = scrollView.frame.size.height
+        
+        for x in 0...2 {
+            let image = UIImage(named: "icon\(x).png")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
+            contentWidth += newX
+            scrollView.addSubview(imageView)
+            imageView.frame = CGRect(x: newX - (frameWidth / 2), y: (view.frame.size.height / 2) - (frameHeight / 2), width: 150, height: 150)
+        }
+        
+        scrollView.clipsToBounds = false
+        scrollView.contentSize = CGSize(width: contentWidth, height: scrollHeight)
     }
-
 
 }
 
